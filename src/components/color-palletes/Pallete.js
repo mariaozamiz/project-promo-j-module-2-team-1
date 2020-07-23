bb9b839dde5359b0f214364cfba9b24bacaa5e5a
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-23 21:56:24 +0200

    add renderPalletes function in Palletes component

--- src/components/color-palletes/Pallete.js
+++ src/components/color-palletes/Pallete.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
-function Pallete() {
+function Pallete(props) {
+    console.log(props);
     return (
         <>
-            <input
-                id="palette-1"
-                type="radio"
-                value="1"
-                name="color-palettes"
-                className="js-palette"
-            />
-            <div className="color-cold-1"></div>
-            <div className="color-cold-2"></div>
-            <div className="color-cold-3"></div>
+            <label
+                className={`palette-${props.number}`}
+                htmlFor={`palette-${props.number}`}
+            >
+                <input
+                    id={`palette-${props.number}`}
+                    type="radio"
+                    value={props.number}
+                    name="color-palettes"
+                    className="js-palette" //estas clases nos las tenemos que cargar, dice Miguel
+                />
+                <div className={props.colors[0]}></div>
+                <div className={props.colors[1]}></div>
+                <div className={props.colors[2]}></div>
+            </label>
         </>
     );
 }
 
 export default Pallete;
+
+// EmailItem.defaultProps = {
+//   from: 'Remitente desconocido'
+// };
--- src/components/color-palletes/Palletes.js
+++ src/components/color-palletes/Palletes.js
@@ -1,56 +1,26 @@
 import React from 'react';
 import Pallete from './Pallete';
 
+const colorPalletes = [
+    ['color-cold-1', 'color-cold-2', 'color-cold-3'],
+    ['color-warm-1', 'color-warm-2', 'color-warm-3'],
+    ['color-neutral-1', 'color-neutral-2', 'color-neutral-3'],
+];
+
 function Palletes() {
+    function renderPalletes() {
+        return colorPalletes.map((element, index) => {
+            return <Pallete key={index} number={index + 1} colors={element} />;
+        });
+    }
     return (
         <>
             <div className="collapsible__content">
                 <legend>Colores</legend>
-                <label className="palette-1" htmlFor="palette-1">
-                    <Pallete />
-                </label>
-                <label className="palette-2" htmlFor="palette-2">
-                    <Pallete />
-                </label>
-                <label className="palette-3" htmlFor="palette-3">
-                    <Pallete />
-                </label>
+                {renderPalletes()}
             </div>
         </>
     );
 }
 
 export default Palletes;
-
-//   <input
-//                 id="palette-1"
-//                 type="radio"
-//                 value="1"
-//                 name="color-palettes"
-//                 className="js-palette"
-//             />
-//             <div className="color-cold-1"></div>
-//             <div className="color-cold-2"></div>
-//             <div className="color-cold-3"></div>
-
-//               <input
-//                 id="palette-2"
-//                 type="radio"
-//                 value="2"
-//                 name="color-palettes"
-//                 className="js-palette"
-//             />
-//             <div className="color-warm-1"></div>
-//             <div className="color-warm-2"></div>
-//             <div className="color-warm-3"></div>
-
-//             <input
-//                 id="palette-3"
-//                 type="radio"
-//                 value="3"
-//                 name="color-palettes"
-//                 className="js-palette"
-//             />
-//             <div className="color-neutral-1"></div>
-//             <div className="color-neutral-2"></div>
-//             <div className="color-neutral-3"></div>
