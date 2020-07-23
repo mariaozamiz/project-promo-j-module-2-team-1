b8da07af78f0b375810ffe614f60c2b04242b63c
Alexandra <sandra.larrosa88@gmail.com>
2020-07-23 15:58:58 +0200

    terminal icons ok

--- src/components/cardPreview/Card.js
+++ src/components/cardPreview/Card.js
@@ -11,7 +11,14 @@ function Card() {
       <div className='profile card__photo js-card-photo'>
         <div className='profile__image js__profile-image'></div>
       </div>
-      <ListIcons />
+      <div className='card__contacts'>
+        <ul className='js-contact__list'>
+          <ListIcons name="js-telephone" hidden='' icon='fas fa-mobile-alt' iconClass='telephone' link=''/>
+          <ListIcons name="js-email" hidden='' icon='far fa-envelope' iconClass='email' link=''/>
+          <ListIcons name="js-linkedin" hidden='' icon='fab fa-linkedin-in' iconClass='linkedin' link=''/>
+          <ListIcons name="js-github" hidden='' icon='fab fa-github-alt' iconClass='github' link='' />
+        </ul>
+    </div>
     </div>
   );
 }
