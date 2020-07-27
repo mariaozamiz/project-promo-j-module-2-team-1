2db09c63145b93dd567be9d36e88fe8ac64207c3
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-07-27 18:26:07 +0200

    improve footer styles

--- src/components/HeaderFooter/Footer.js
+++ src/components/HeaderFooter/Footer.js
@@ -3,11 +3,11 @@ import footerLogo from '../../images/logo-adalab.png';
 
 function Footer() {
   return (
-    <footer className='footer_design'>
+    <footer className='footer'>
       <div className='wrapper'>
-        <a href='./index.html'>
-          <span className='copyright'>Awesome profile-cards @2020</span>
-          <img className='logo__photo' src={footerLogo} alt='logo-adalab' />
+        <span className='copyright'>Awesome profile-cards @2020</span>
+        <a href='https://adalab.es/' target='_blank'>
+          <img className='logo-adalab' src={footerLogo} alt='logo-adalab' />
         </a>
       </div>
     </footer>
