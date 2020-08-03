66bd02221e40a4a9011803354e4d9e2d72965494
Alexandra <sandra.larrosa88@gmail.com>
2020-08-03 14:17:19 +0200

    create commponent landing and add jsx

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
-function Landing() {
-  return <div>
-    
-  </div>;
-}
+const Landing = (props) => {
+  return (
+    <div>
+      <header class='header'>
+        <div class='wrapper'>
+          <img
+            class='header__photo'
+            src='./assets/images/tarjetas-molonas.png'
+          />
+        </div>
+      </header>
+
+      <main class='main'>
+        <div class='wrapper'>
+          <h1>Crea tu tarjeta de visita</h1>
+          <p>Crea mejores contactos profesionales de forma fácil y cómoda</p>
+          <section class='options'>
+            <article>
+              <i class='far fa-object-ungroup'></i>
+              <p>Diseña</p>
+            </article>
+            <article>
+              <i class='far fa-keyboard'></i>
+              <p>Rellena</p>
+            </article>
+            <article>
+              <i class='fas fa-share-alt'></i>
+              <p>Comparte</p>
+            </article>
+          </section>
+          <div class='button__start'>
+            <a href='./design.html' class='button__start--text'>
+              comenzar
+            </a>
+          </div>
+        </div>
+      </main>
+      <footer class='footer'>
+        <div class='wrapper'>
+          <span class='copyright'>Awesome profile-cards @2018</span>
+          <img
+            class='logo-adalab'
+            src='././assets/images/logo-adalab.png'
+            rel='logo-adalab'
+          />
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 //import "./stylesheets/core/reset.scss";
-import Design from "./components/Design";
+import Design from './components/Design';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Design />
+    <HashRouter>
+      <Design />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
