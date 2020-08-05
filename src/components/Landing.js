2a08bbfee6c54885b6c3181760eff6ee41234ab4
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-05 22:15:58 +0200

    add link twiiter and card

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import '../stylesheets/layout/landing/main.scss';
-import headerLogo from '../images/tarjetas-molonas.png';
-import { Link } from 'react-router-dom';
-import Footer from "./HeaderFooter/Footer"
+import React from "react";
+import "../stylesheets/layout/landing/main.scss";
+import headerLogo from "../images/tarjetas-molonas.png";
+import { Link } from "react-router-dom";
+import Footer from "./HeaderFooter/Footer";
 
 const Landing = (props) => {
   return (
     <div>
-      <header className='header'>
-        <div className='wrapper'>
-          <img className='header__photo' src={headerLogo} />
+      <header className="header">
+        <div className="wrapper">
+          <img className="header__photo" src={headerLogo} alt="header logo" />
         </div>
       </header>
 
-      <main className='main'>
-        <div className='wrapper'>
+      <main className="main">
+        <div className="wrapper">
           <h1>Crea tu tarjeta de visita</h1>
           <p>Crea mejores contactos profesionales de forma fácil y cómoda</p>
-          <section className='options'>
+          <section className="options">
             <article>
-              <i className='far fa-object-ungroup'></i>
+              <i className="far fa-object-ungroup"></i>
               <p>Diseña</p>
             </article>
             <article>
-              <i className='far fa-keyboard'></i>
+              <i className="far fa-keyboard"></i>
               <p>Rellena</p>
             </article>
             <article>
-              <i className='fas fa-share-alt'></i>
+              <i className="fas fa-share-alt"></i>
               <p>Comparte</p>
             </article>
           </section>
-          <div className='button__start'>
-            <Link to='/design' className='button__start--text'>
+          <div className="button__start">
+            <Link to="/design" className="button__start--text">
               comenzar
             </Link>
           </div>
--- src/components/shareComponents/ShareContent.js
+++ src/components/shareComponents/ShareContent.js
@@ -6,8 +6,13 @@ import "../../stylesheets/layout/shareContent/share.scss";
 function ShareContent(props) {
   return (
     <div className="collapsible__content">
-      <ShareButton />
-      <TwitterContent iconTwitter={props.iconTwitter} />
+      <ShareButton SendData={props.SendData} />
+      <TwitterContent
+        iconTwitter={props.iconTwitter}
+        cardURL={props.cardURL}
+        twitterLink={props.twitterLink}
+        openOutside={props.openOutside}
+      />
     </div>
   );
 }
