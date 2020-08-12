0f86e5071ee424eb2c1932b25433bfe43a239178
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-08-12 15:03:22 +0200

    add run docs

--- docs/precache-manifest.54d1bfa56f8d1beee87f2384c62eee2c.js
+++ docs/precache-manifest.54d1bfa56f8d1beee87f2384c62eee2c.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16efb94d5089754b32ab91353d35e754",
+    "revision": "5cf89bfc66bc8b0e96856b0929d98f07",
     "url": "./index.html"
   },
   {
-    "revision": "67cb86cd6357ebf009d1",
+    "revision": "c7b1fdb710ef5db140f3",
     "url": "./static/css/main.e78907a6.chunk.css"
   },
   {
-    "revision": "c9a9dd1bfca6c2118248",
-    "url": "./static/js/2.e4b710b0.chunk.js"
+    "revision": "35153924dbfcbf15044c",
+    "url": "./static/js/2.86bbe0fb.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.e4b710b0.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.86bbe0fb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "67cb86cd6357ebf009d1",
-    "url": "./static/js/main.ce1273a4.chunk.js"
+    "revision": "c7b1fdb710ef5db140f3",
+    "url": "./static/js/main.8240b142.chunk.js"
   },
   {
     "revision": "39d4975f2cd8222075bb",
     "url": "./static/js/runtime-main.baea389b.js"
+  },
+  {
+    "revision": "822e78314deaad22a4e8e7590ef9c81c",
+    "url": "./static/media/malefica.822e7831.jpg"
   },
   {
     "revision": "d32fd9ba662a480121f3c1653d74f771",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2203eebcaa78707bff2e12bdf55c2ee0.js"
+  "./precache-manifest.54d1bfa56f8d1beee87f2384c62eee2c.js"
 );
 
 self.addEventListener('message', (event) => {
